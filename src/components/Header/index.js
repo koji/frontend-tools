e422b9ed4dce173b8caddd122e3c10771189cd3d
koji <kk2796@nyu.edu>
2021-08-02 14:03:19 -0400

    update color

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,9 +7,10 @@ export const Header = () => {
       <Link href="/">
         <a className={styles.link}>
           <h1 className={styles.headers}>
-            <span className={styles.header__title__first}>Front</span>
+            {/* <span className={styles.header__title__first}>Front</span>
             <span className={styles.header__title__second}>End</span>
-            <span className={styles.header__title__third}>Tools</span>
+            <span className={styles.header__title__third}>Tools</span> */}
+            <span className={styles.header__title}>FrontEnd Tools</span>
           </h1>
           <h2 className={`${styles.headers} ${styles.header__subtitle}`}>
             Spread Joy & Usefulness
