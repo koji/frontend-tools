3dcacd25e1264aa0ddfee5ce4c7bc62a886f5cf4
koji <kk2796@nyu.edu>
2021-08-04 13:55:00 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,38 +1,38 @@
-import Link from "next/link";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+// import Link from "next/link";
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
 
-export default NotFound = () => {
-  const router = useRouter();
+// export default function NotFound() {
+//   const router = useRouter();
 
-  useEffect(() => {
-    setTimeout(() => {
-      router.push("/");
-    }, 4000);
-  }, []);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       router.push("/");
+//     }, 4000);
+//   }, []);
 
-  return (
-    <div className="not-found">
-      <h1>404</h1>
-      <h2>That page cannot be found :(</h2>
-      <p>
-        redirecting to the <Link href="/">home</Link>
-      </p>
+//   return (
+//     <div className="not-found">
+//       <h1>404</h1>
+//       <h2>That page cannot be found :(</h2>
+//       <p>
+//         redirecting to the <Link href="/">home</Link>
+//       </p>
 
-      <style.jsx>
-        {" "}
-        {`
-        .not-found {
-            background-color: #fff;
-            padding: 20px;
-            box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
-            trasform: rotateZ(-1deg);
-        }
-        h1 {
-            font-size: 3em;
-        }
-        `}
-      </style.jsx>
-    </div>
-  );
-};
+//       <style.jsx>
+//         {" "}
+//         {`
+//         .not-found {
+//             background-color: #fff;
+//             padding: 20px;
+//             box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+//             transform: rotateZ(-1deg);
+//         }
+//         h1 {
+//             font-size: 3em;
+//         }
+//         `}
+//       </style.jsx>
+//     </div>
+//   );
+// }
