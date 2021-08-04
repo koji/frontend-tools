d6d1da7b6a29bbd35db1d8432465d9755332ff28
koji <kk2796@nyu.edu>
2021-08-04 13:39:50 -0400

    redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 4000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>404</h1>
