444a5210c88a21d53ed398be3ad0a1b565def045
koji <kk2796@nyu.edu>
2021-08-05 19:42:56 -0400

    add meta tag to SEO component

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -12,6 +12,12 @@ export const SEO = ({ title }) => {
         property="og:title"
         content="fetools - A collection of tools for front-end developers."
       />
+      <meta property="og:url" content="https://fetools.vercel.app/" />
+      <meta
+        property="og:image"
+        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
+      />
+
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
