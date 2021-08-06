fce32d38b0b2e0856c3fd1ebc204fe5860e71538
koji <kk2796@nyu.edu>
2021-08-06 02:58:06 -0400

    remove unused lines

--- src/components/PageSEO/index.js
+++ src/components/PageSEO/index.js
@@ -36,30 +36,6 @@ export const PageSEO = ({ title }) => {
         content="fetools - A collection of tools for front-end developers. Offer useful information for web development!"
       />
       <meta property="twitter:image" content={"/thumbnail.jpg"} />
-      {/* <meta
-        name="description"
-        content="A collection of tools for front-end developers."
-      />
-      <meta
-        property="og:title"
-        content="fetools - A collection of tools for front-end developers."
-      />
-      <meta property="og:url" content="https://fetools.vercel.app/" />
-      <meta
-        property="og:image"
-        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@kojikanao_nyc" />
-      <meta name="twitter:title" content="FRONTEND TOOLS" />
-      <meta
-        name="twitter:description"
-        content="fetools - A collection of tools for front-end developers. HTML/CSS/JS/TS & Image"
-      />
-      <meta
-        name="twitter:image"
-        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
-      /> */}
     </Head>
   );
 };
