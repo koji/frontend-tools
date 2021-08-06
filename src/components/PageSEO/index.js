5f013ebc287ffcf0aeded9c9f2366884d31525ef
koji <kk2796@nyu.edu>
2021-08-06 03:52:41 -0400

    Update index.js

--- src/components/PageSEO/index.js
+++ src/components/PageSEO/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import thumbnail from "../../../public/images/thumbnail.jpg";
 
 export const PageSEO = ({ title }) => {
   return (
@@ -24,7 +23,10 @@ export const PageSEO = ({ title }) => {
         property="og:description"
         content="fetools - A collection of tools for front-end developers. Offer useful information for web development!"
       />
-      <meta property="og:image" content={thumbnail} />
+      <meta
+        property="og:image"
+        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
+      />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://fetools.vercel.app/" />
@@ -36,7 +38,10 @@ export const PageSEO = ({ title }) => {
         property="twitter:description"
         content="fetools - A collection of tools for front-end developers. Offer useful information for web development!"
       />
-      <meta property="twitter:image" content={thumbnail} />
+      <meta
+        property="twitter:image"
+        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
+      />
     </Head>
   );
 };
