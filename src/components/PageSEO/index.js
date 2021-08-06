3a67b796ad9b79b3d0284459458fa78fd0b574c2
koji <kk2796@nyu.edu>
2021-08-06 00:21:49 -0400

    Add twitter card

--- src/components/PageSEO/index.js
+++ src/components/PageSEO/index.js
@@ -17,6 +17,17 @@ export const PageSEO = ({ title }) => {
         property="og:image"
         content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
       />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@kojikanao_nyc" />
+      <meta name="twitter:title" content="FRONTEND TOOLS" />
+      <meta
+        name="twitter:description"
+        content="fetools - A collection of tools for front-end developers. HTML/CSS/JS/TS & Image"
+      />
+      <meta
+        name="twitter:image"
+        content="https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg"
+      />
     </Head>
   );
 };
