e4140992e4512e520259061aa83d2e0885454b47
koji <kk2796@nyu.edu>
2021-08-09 13:26:10 -0400

    update SEO

--- site.config.js
+++ site.config.js
@@ -1,7 +1,13 @@
 export const config = {
   siteMeta: {
     title: "Frontend Tools",
+    author: "koji kanao",
     url: "https://fetools.vercel.app/",
-    description: "",
+    subTitle: "A collection of tools for front-end developers",
+    description:
+      "A collection of tools for front-end developers. Offer useful information for web development!",
+    image:
+      "https://user-images.githubusercontent.com/474225/128434919-88ab6e5a-e23e-4eba-bc91-4f4b4e01e95f.jpg",
+    gsc: "Ulam6Eml3N7P9x88IViUfNV5xG3HXRWxLpZ-5cp0xaU",
   },
 };
