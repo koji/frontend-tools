471e1360346b3a3ba539cca40036a1481f16ba94
koji <kk2796@nyu.edu>
2021-08-20 07:56:08 -0400

    update for Accessibility

--- src/components/HeaderLink/index.tsx
+++ src/components/HeaderLink/index.tsx
@@ -5,7 +5,7 @@ import styles from './HeaderLink.module.css';
 export const HeaderLink = () => (
   <div className={styles.container}>
     <Link href='https://github.com/koji/frontend-tools'>
-      <a target='_blank' rel='noopener'>
+      <a target='_blank' rel='noopener' aria-label='Link to GitHub repo'>
         <GoMarkGithub className={styles.icon} />
       </a>
     </Link>
