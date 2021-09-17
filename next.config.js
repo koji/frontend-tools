aeda5e5bb0eccfd3539d0c99fe4fe0a0e96a2500
koji <kk2796@nyu.edu>
2021-09-17 05:04:44 -0400

    add paginate

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     domains: ['images.ctfassets.net'],
   },
   eslint: {
-    dirs: ['pages', 'components', 'lib'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['pages', 'components', 'lib', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
   basePath: '',
   reactStrictMode: true,
